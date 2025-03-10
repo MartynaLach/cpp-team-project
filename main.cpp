ff7436686aa194f3b55d0659dd020671571374dd
MartynaLach <127475569+MartynaLach@users.noreply.github.com>
2025-03-10 16:20:13 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+
+# cpp-team-project
+
+
+#include <iostream>
+using namespace std;
+
+int subtract(int a, int b) {
+    return a - b;
+}
+  
+int multiply(int a, int b) {
+    return a * b;
+}
+  int add(int a, int b) {
+    return a + b;
+}
+
+
+  
+ 
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+  cout << "Witaj w naszym programie!" << endl;
+  t << "2 + 3 = " << add(2, 3) << endl;
+  cout << "5 - 3 = " << subtract(5, 3) << endl;
+  cout << "2 * 3 = " << multiply(2, 3) << endl;
+  c
+  return 0;
+} 
+
+
